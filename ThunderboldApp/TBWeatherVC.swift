17815e7e1e8466daa099a7a797cd9da8145558aa
mfsaglam <mfsaglam@yandex.com>
2022-09-14 20:03:21 +0300

    add location button

--- ThunderboldApp/TBWeatherVC.swift
+++ ThunderboldApp/TBWeatherVC.swift
@@ -10,6 +10,7 @@ import UIKit
 class TBWeatherVC: UIViewController {
     
     var screenTitle = UILabel()
+    var locationButton = UIButton()
     var cityTextField = TBTextField()
     
 
@@ -29,24 +30,39 @@ class TBWeatherVC: UIViewController {
         navigationController?.setNavigationBarHidden(true, animated: false)
         
         view.addSubview(screenTitle)
+        view.addSubview(locationButton)
         view.addSubview(cityTextField)
         
         screenTitle.text = "Search for city"
         screenTitle.font = UIFont.systemFont(ofSize: 18, weight: .medium)
         screenTitle.textAlignment = .center
         
+        let locationButtonConfig = UIImage.SymbolConfiguration(pointSize: 25, weight: .light)
+        locationButton.setImage(UIImage(systemName: "location.circle", withConfiguration: locationButtonConfig), for: .normal)
+        locationButton.addTarget(self, action: #selector(locationTapped), for: .touchUpInside)
+    
         screenTitle.translatesAutoresizingMaskIntoConstraints = false
+        locationButton.translatesAutoresizingMaskIntoConstraints = false
         cityTextField.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             screenTitle.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
             screenTitle.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             
+            locationButton.centerYAnchor.constraint(equalTo: screenTitle.centerYAnchor),
+            locationButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            locationButton.heightAnchor.constraint(equalToConstant: 50),
+            locationButton.widthAnchor.constraint(equalToConstant: 50),
+            
             cityTextField.topAnchor.constraint(equalTo: screenTitle.bottomAnchor, constant: 30),
             cityTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
             cityTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
             cityTextField.heightAnchor.constraint(equalToConstant: 50)
         ])
+    }
+    
+    @objc private func locationTapped() {
+        //get current location and get weather data
     }
 
 }
