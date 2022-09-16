28c3b42b7ec143a28818887414b8d0a6f12ea8f1
mfsaglam <mfsaglam@yandex.com>
2022-09-16 19:56:13 +0300

    add weatherData

---
+++ ThunderboldApp/Weather.swift
@@ -0,0 +1,25 @@
+//
+//  Weather.swift
+//  ThunderboldApp
+//
+//  Created by Fatih Sağlam on 16.09.2022.
+//
+
+import Foundation
+
+struct Weather: Codable {
+    var location: Location?
+    var current: Current?
+}
+
+struct Location: Codable {
+    var name: String?
+}
+
+struct Current: Codable {
+    var temperature: Int?
+    var weatherDescriptions: [String]?
+    var weatherCode: Int?
+    var windSpeed: Int?
+    var pressure, humidity: Int?
+}
