dfec1f97d58b7c918bd48c2438be6f40fc335ee5
mfsaglam <mfsaglam@yandex.com>
2022-09-17 19:18:42 +0300

    add weather icons, add weather manager

---
+++ ThunderboldApp/Managers/WeatherManager.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherManager.swift
+//  ThunderboldApp
+//
+//  Created by Fatih Sağlam on 17.09.2022.
+//
+
+import UIKit
+
+struct WeatherManager {
+    
+    func getConditionImage(weatherCode: Int) -> UIImage {
+
+    }
+    
+}
